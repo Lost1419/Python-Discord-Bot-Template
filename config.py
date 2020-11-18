516627ca9338c4ece8dfbacfd1968c5611417050
Lost <lostfrost1419@gmail.com>
2020-11-18 15:20:04 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Can be multiple prefixes, like this: ("!", "?")
 BOT_PREFIX = ("!")
-TOKEN = "Nzc3NjE0Mzc0NzkyODU1NjAy.X7F_0g.5fqc19cJpruyFPXUMrwkUUmEB0Y"
-APPLICATION_ID = "777614374792855602"
+TOKEN = ""
+APPLICATION_ID = ""
 OWNERS = [123456789, 987654321]
 BLACKLIST = []
  # Default cogs that I have created for the template
